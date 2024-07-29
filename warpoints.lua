0cef22de41f02f16455230eb2e1f1f9ae992e0b9
xypher <84131859+xxypher@users.noreply.github.com>
2024-07-29 21:03:26 +0300

    Update warpoints.lua

--- warpoints.lua
+++ warpoints.lua
@@ -2,21 +2,21 @@ return function(Object: Instance)
 	local Light: PointLight = Object:FindFirstChildWhichIsA('PointLight')
 	local Mesh: SpecialMesh = Object:FindFirstChildWhichIsA("SpecialMesh")
 	local Particle: ParticleEmitter = Object:FindFirstChildWhichIsA('ParticleEmitter', true)
-	
+
 	if Object:IsA('MeshPart') then
-			
-		end
+
+	
 	elseif Object:IsA('Part') then
-		
-		end
+
+
 	elseif Object:IsA('UnionOperation') then
 		if Object.Color == Color3.fromRGB(211, 0, 0) then
 			return {
 				Color = Color3.fromRGB(211, 0, 0),
 				Name = 'Tome of War'
 			}
-		end
 	end
-	
-	return nil
+end
+
+return nil
 end
